12b8f9e32e8bf331968f4283e499048f49a9a52c
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-05-12 16:56:02 +0500

    remending page add and code fixed

---
+++ src/components/Remending.vue
@@ -0,0 +1,40 @@
+<script>
+  import reminding8492 from "@/assets/captcha/get/reminding8492.jpg"
+  export default {
+    data:function (){
+      return {
+        reminding8492: reminding8492,
+      }
+    }
+  }
+</script>
+
+<template>
+  <section class="inner_page">
+    <div class="container">
+      <h1>Восстановление доступа</h1>
+      <form method="POST" action="#" class="reminding_form ajax">
+        <input type="hidden" name="action" value="mail">
+        <input type="hidden" name="module" value="reminding">
+        <div class="infofield">Введите ваш e-mail<span style="color:red;">*</span>:</div>
+        <input type="email" name="mail" value=""><br>
+        <div class="errors error_mail" style="display:none"></div>
+        <div class="captcha"><div class="block captcha">
+          <img :src="reminding8492" width="159" height="80" class="code_img captcha-image">
+          <input type="hidden" name="captchaint" value="8492">
+          <input type="hidden" name="captcha_update" value="">
+          <span class="input-title">Введите код с картинки:</span>
+          <input type="text" name="captcha" value="" autocomplete="off">
+          <br>
+          <div class="js_captcha_update captcha_update"><a href="javascript:void(0)"><i class="fas fa-redo">&nbsp;</i></a></div></div><div class="errors error_captcha" style="display:none"></div></div>
+        <input type="submit" value="Отправить">
+        <div class="required_field"><span style="color:red;">*</span> — Поля, обязательные для заполнения</div>
+      </form>
+      <div class="errors error reminding_result" style="display:none"></div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/RemendingView.vue
+++ src/views/RemendingView.vue
@@ -1,9 +1,18 @@
 <script setup>
 
+import Top from "@/components/Layout/Top.vue";
+import Footer from "@/components/Home/Footer.vue";
+import Remending from "@/components/Remending.vue";
+
 </script>
 
 <template>
-  <h2>RemendingView</h2>
+  <main>
+    <Top />
+    <Remending />
+  </main>
+
+  <Footer />
 </template>
 
 <style scoped>
